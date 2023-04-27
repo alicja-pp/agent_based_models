deb5f707d84b28f64cb49c019e6c0b1ff45bd39a
Dawid Karp <dadulerk@gmail.com>
2023-04-27 16:58:33 +0200

    add SIR model simulation, add BA graph generation

--- agents.cpp
+++ agents.cpp
@@ -1,6 +1,8 @@
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <random>
+#include <set>
 #include <vector>
 
 #include "TAxis.h"
@@ -14,89 +16,106 @@
 using namespace std;
 
 struct Edge {
-    long to;
+    int to;
+
+    bool operator==(const Edge& other) const {
+        return to == other.to;
+    }
 };
+
+typedef vector<Edge> Edges;
 
 enum class Node { SUSCEPTIBLE, INFECTED, RESISTANT };
 
 struct Graph {
     vector<Node> nodes;
-    const vector<vector<Edge>> edges;
+    const vector<Edges> adj_list;
 };
 
-Graph generate_ER(long n, double p) {
+Graph generate_ER(int n, double p) {
     vector<Node> nodes(n);
-    vector<vector<Edge>> edges;
+    vector<Edges> adj_list;
 
-    for (long i = 0; i < n; i++) {
-        vector<Edge> neighbors;
+    for (int i = 0; i < n; i++) {
+        Edges edges;
 
-        for (long j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             if ((double)rand() / RAND_MAX < p && i != j) {
-                neighbors.push_back(Edge{.to = j});
+                edges.push_back(Edge{.to = j});
             }
         }
 
-        edges.push_back(neighbors);
+        adj_list.push_back(edges);
     }
 
-    return Graph{.nodes = nodes, .edges = edges};
+    return Graph{.nodes = nodes, .adj_list = adj_list};
 }
 
-Graph generate_BA(long n, double m) {
+Graph generate_BA(int n, int m) {
+    vector<int> node_indices;
+    iota(begin(node_indices), end(node_indices), 0);
+
+    vector<Edges> adj_list;
+
+    int rand_node_index;
+
+    for (int i = 0; i < n; i++) {
+        Edges edges;
+
+        for (int k = 0; k < m; k++) {
+            Edge edge;
+
+            do {
+                rand_node_index = rand() % n;
+                edge.to = rand_node_index;
+
+            } while (find(edges.begin(), edges.end(), edge) != edges.end());
+
+            edges.push_back(edge);
+            node_indices.push_back(rand_node_index);
+        }
+
+        adj_list.push_back(edges);
+    }
+
     vector<Node> nodes(n);
-    vector<vector<Edge>> edges;
-
-    for (long i = 0; i < n; i++) {
-        vector<Edge> neighbors;
-
-        // TODO
-
-        edges.push_back(neighbors);
-    }
-
-    return Graph{.nodes = nodes, .edges = edges};
+    return Graph{.nodes = nodes, .adj_list = adj_list};
 }
 
-void simulate_SI(Graph &graph, double beta, long initial_infected) {
+void simulate_SI(Graph graph, double beta, int initial_infected) {
     // starting with some (infected_nr) random infected nodes
-    const long n = graph.nodes.size();
-    for (int i = 0; i < initial_infected; i++) {
+    const int N = graph.nodes.size();
+    for (int i = 0; i < initial_infected; i++)
         graph.nodes.at(i) = Node::INFECTED;
-    }
-    for (int i = initial_infected; i < n; i++) {
+
+    for (int i = initial_infected; i < N; i++)
         graph.nodes.at(i) = Node::SUSCEPTIBLE;
-    }
 
     // simulate and show
-    TCanvas *si_canvas = new TCanvas("si_canvas", "Graph", 1200, 800);
+    TCanvas *si_canvas = new TCanvas("si_canvas", "SI model", 1200, 800);
     TGraph *infected_graph = new TGraph();
     TGraph *susceptible_graph = new TGraph();
 
-    infected_graph->SetTitle("SI;Time;Number of infected nodes");
+    infected_graph->SetTitle("SI;Step");
 
     long step = 0;
 
     int infected = initial_infected;
     int node_i_neighbors, rand_neighbor, j;
 
-    while (max(infected, infected - 100) < n) {
+    while (max(infected, infected - 100) < N) {
         infected_graph->SetPoint(step, step, infected);
-        susceptible_graph->SetPoint(step, step, n - infected);
+        susceptible_graph->SetPoint(step, step, N - infected);
 
         // iterate through all nodes
-        for (int i = 0; i < n; ++i) {
-            node_i_neighbors = graph.edges.at(i).size();
+        for (int i = 0; i < N; ++i) {
+            node_i_neighbors = graph.adj_list.at(i).size();
 
-            if (node_i_neighbors == 0) {
-                cout << "i = " << i << ", with k = " << node_i_neighbors
-                     << endl;
-                continue;
-            }
+            if (node_i_neighbors == 0) continue;
 
             rand_neighbor = rand() % node_i_neighbors;
 
-            j = graph.edges.at(i).at(rand_neighbor).to;
+            j = graph.adj_list.at(i).at(rand_neighbor).to;
 
             if (((double)rand() / RAND_MAX) < beta) {
                 if (graph.nodes.at(i) == Node::INFECTED &&
@@ -117,12 +136,105 @@ void simulate_SI(Graph &graph, double beta, long initial_infected) {
     susceptible_graph->Draw("same");
 }
 
+void simulate_SIR(Graph graph, double beta, double gamma, int initial_infected,
+                  int initial_resitant) {
+    const int N = graph.nodes.size();
+
+    for (int i = 0; i < initial_infected; i++)
+        graph.nodes.at(i) = Node::INFECTED;
+
+    for (int i = initial_infected; i < initial_infected + initial_resitant;
+         i++)
+        graph.nodes.at(i) = Node::RESISTANT;
+
+    for (int i = initial_infected + initial_resitant; i < N; i++)
+        graph.nodes.at(i) = Node::SUSCEPTIBLE;
+
+    // simulate and show
+    TCanvas *sir_canvas = new TCanvas("sir_canvas", "SIR model", 1200, 800);
+    TGraph *susceptible_graph = new TGraph();
+    TGraph *infected_graph = new TGraph();
+    TGraph *resistant_graph = new TGraph();
+
+    infected_graph->SetTitle("SIR;Step");
+
+    long step = 0;
+
+    int infected = initial_infected;
+    int resistant = initial_resitant;
+
+    int node_i_neighbors, rand_neighbor, j;
+
+    bool should_infect, should_resist;
+
+    while (infected > 0) {
+        cout << N - resistant - infected << ", " << resistant << ", "
+             << infected << "\n";
+
+        susceptible_graph->SetPoint(step, step, N - resistant - infected);
+        infected_graph->SetPoint(step, step, infected);
+        resistant_graph->SetPoint(step, step, resistant);
+
+        // iterate through all nodes
+        for (int i = 0; i < N; ++i) {
+            node_i_neighbors = graph.adj_list.at(i).size();
+
+            if (node_i_neighbors == 0) continue;
+
+            j = graph.adj_list.at(i).at(rand() % node_i_neighbors).to;
+
+            should_infect = ((double)rand() / RAND_MAX) < beta;
+            should_resist = ((double)rand() / RAND_MAX) < gamma;
+
+            if (should_infect && graph.nodes.at(i) == Node::INFECTED &&
+                graph.nodes.at(j) == Node::SUSCEPTIBLE) {
+                graph.nodes.at(j) = Node::INFECTED;
+                infected++;
+
+            } else if (should_infect && graph.nodes.at(j) == Node::INFECTED &&
+                       graph.nodes.at(i) == Node::SUSCEPTIBLE) {
+                graph.nodes.at(i) = Node::INFECTED;
+                infected++;
+
+            } else if (should_resist && graph.nodes.at(i) == Node::INFECTED &&
+                       graph.nodes.at(j) == Node::INFECTED) {
+                graph.nodes.at(i) = Node::RESISTANT;
+                infected--;
+                resistant++;
+
+            } else if (should_resist && graph.nodes.at(i) == Node::INFECTED &&
+                       graph.nodes.at(j) == Node::RESISTANT) {
+                graph.nodes.at(i) = Node::RESISTANT;
+                infected--;
+                resistant++;
+
+            } else if (should_resist && graph.nodes.at(j) == Node::INFECTED &&
+                       graph.nodes.at(i) == Node::RESISTANT) {
+                graph.nodes.at(j) = Node::RESISTANT;
+                infected--;
+                resistant++;
+            }
+        }
+
+        step++;
+    }
+
+    susceptible_graph->GetYaxis()->SetRangeUser(0, N);
+    susceptible_graph->Draw();
+
+    infected_graph->GetYaxis()->SetRangeUser(0, N);
+    infected_graph->Draw("same");
+
+    resistant_graph->GetYaxis()->SetRangeUser(0, N);
+    resistant_graph->Draw("same");
+}
+
 void show_graph(const Graph &graph) {
     TCanvas *canvas = new TCanvas("canvas", "Graph", 800, 800);
     int max_coordinate = 10;
 
-    const long n = graph.nodes.size();
-    TGraph *agents = new TGraph(n);
+    const int N = graph.nodes.size();
+    TGraph *agents = new TGraph(N);
 
     canvas->Range(-max_coordinate - 2., -max_coordinate - 2.,
                   max_coordinate + 2, max_coordinate + 2);
@@ -138,7 +250,7 @@ void show_graph(const Graph &graph) {
     agents->GetYaxis()->SetTickLength(0);
 
     double rand1, rand2;
-    for (long i = 0; i < n; ++i) {
+    for (int i = 0; i < N; ++i) {
         do {
             rand1 = sqrt((double)rand() / RAND_MAX) * max_coordinate;
             rand2 = ((double)rand() / RAND_MAX) * 2 * M_PI;
@@ -150,12 +262,12 @@ void show_graph(const Graph &graph) {
     vector<TLine *> connections;
 
     // iterate through all nodes
-    for (long i = 0; i < n; ++i) {
+    for (int i = 0; i < N; ++i) {
         agents->GetPoint(i, x1, y1);
 
         // iterate through all i-th node's edges
-        for (long j = 0; j < graph.edges.at(i).size(); ++j) {
-            agents->GetPoint(graph.edges.at(i).at(j).to, x2, y2);
+        for (int j = 0; j < graph.adj_list.at(i).size(); ++j) {
+            agents->GetPoint(graph.adj_list.at(i).at(j).to, x2, y2);
 
             // draw line between i-th node and j-th node
             connections.push_back(new TLine(x1, y1, x2, y2));
@@ -169,8 +281,12 @@ void show_graph(const Graph &graph) {
 void agents() {
     srand(time(NULL));
 
-    Graph ER_graph = generate_ER(1000, 0.008);
+    // Graph ER_graph = generate_ER(1000, 0.008);
+    Graph BA_graph = generate_BA(100, 3);
 
-    simulate_SI(ER_graph, 0.05, 1);
-    show_graph(ER_graph);
+    // simulate_SI(ER_graph, 0.05, 1);
+    // simulate_SIR(ER_graph, 0.1, 0.05, 10, 0);
+    simulate_SI(BA_graph, 0.5, 1);
+
+    show_graph(BA_graph);
 }
